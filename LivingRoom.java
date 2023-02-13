c5b85dab2dd00b39e04a5a3e424c37ec9a0d51e7
livcarr <123025829+livcarr@users.noreply.github.com>
2023-02-13 13:28:22 -0500

    Update and rename class3.java to LivingRoom.java

---
+++ LivingRoom.java
@@ -0,0 +1,10 @@
+class LivingRoom
+{
+  livingRoom()
+    {
+      int size = 1300;
+      String color = "Brown and black";
+      System.out.println("Size: " + printSqFt(size));
+      System.out.println("Color scheme: " + printColorScheme(color));
+    }
+}
