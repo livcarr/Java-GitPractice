362bb5f44f201e9f9aadf63bbdda33a5c5389e84
livcarr <123025829+livcarr@users.noreply.github.com>
2023-02-13 13:25:25 -0500

    Update and rename class2.java to Bedroom.java

---
+++ Bedroom.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Bedroom implements Room
+{
+  bedroom()
+  {
+    int size = 135;
+    String color = "Yellow and gray";
+    System.out.println("Size: " + printSqFt(size));
+    System.out.println("Color scheme: " + printColorScheme(color));
+    }
+  
+}
